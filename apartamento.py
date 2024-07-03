9f0ad906e83b1977db9deda37fe2adcad3ed98d1
Thiago Schiedeck <103116333+oThisgo@users.noreply.github.com>
2024-07-03 09:38:42 -0300

    Add files via upload

---
+++ apartamento.py
@@ -0,0 +1,11 @@
+class Apartamento():
+    def __init__(self, id, numero):
+        self.id = id
+        self.numero = numero
+        self.torre = None
+        self.vaga = None
+        self.proximo = None
+
+    def cadastrar():
+
+    def imprimir():
---
+++ torre.py
@@ -0,0 +1,9 @@
+class Torre():
+    def __init__(self, id, nome, endereco):
+        self.id = id
+        self.nome = nome
+        self.endereco = endereco
+    
+    def cadastrar():
+
+    def imprimir():
